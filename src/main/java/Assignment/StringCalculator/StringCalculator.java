42dcc3b4b128826cb2d4b12186bb59544d313c67
Rajkumar Viradiya <rajviradiya5197@gmail.com>
2021-01-08 17:21:01 +0530

    support for different delemiter

--- src/main/java/Assignment/StringCalculator/StringCalculator.java
+++ src/main/java/Assignment/StringCalculator/StringCalculator.java
@@ -9,6 +9,9 @@ public class StringCalculator
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add("//+\n1+2\n3"));
     }
 
 	public int add(String string) {
@@ -16,15 +19,33 @@ public class StringCalculator
 			return 0;
 		else{
 			int result = 0;
-			string = string.replace("\n", ",");
-			String[] numbers = string.split(",");
-			if(numbers.length == 1){
-				result =  Integer.parseInt(numbers[0]);
-			}else{
-				for(String num : numbers){
-					result = result + Integer.parseInt(num);
-				}		
+			if(string.startsWith("//")){
+				String[] container = string.substring(2).split("\n");
+				String delemiter = container[0];
+				String numberString = container[1];
+				string = numberString.replace("\n", "\\" + delemiter);
+				String[] numbers = numberString.split("\\" + delemiter);
+				if(numbers.length == 1){
+					result =  Integer.parseInt(numbers[0]);
+				}else{
+					for(String num : numbers){
+						result = result + Integer.parseInt(num);
+					}		
+				}	
 			}
+			else{
+				
+				string = string.replace("\n", ",");
+				String[] numbers = string.split(",");
+				if(numbers.length == 1){
+					result =  Integer.parseInt(numbers[0]);
+				}else{
+					for(String num : numbers){
+						result = result + Integer.parseInt(num);
+					}		
+				}	
+			}
+			
 			return result;
 		}
 	}
--- src/test/java/Assignment/StringCalculator/StringCalculatorTest.java
+++ src/test/java/Assignment/StringCalculator/StringCalculatorTest.java
@@ -24,5 +24,8 @@ public class StringCalculatorTest
 		assertEquals(18, calculator.add("2,3,6,7"));
 		assertEquals(6, calculator.add("1\n2,3"));
 		assertEquals(10, calculator.add("1\n2,3\n4"));
+		assertEquals(3, calculator.add("//;\n1;2"));
+		assertEquals(3, calculator.add("//*\n1*2"));
+		assertEquals(24, calculator.add("//+\n1+23"));
 	}
 }
